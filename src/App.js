12b5b956be1017c6300b9db5c6fa44d555e5b5f4
unknown <skj99667788@gmail.com>
2022-10-01 14:50:52 +0500

    component set

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import SideBar from './Components/SideBar';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <SideBar/>
+    </div>
+    </>
+  );
+}
+
+export default App;
