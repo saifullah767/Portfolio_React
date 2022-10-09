90b3b5a4090ef07e6904fa2b8366f1fea6c200d5
FIRST_NAME LAST_NAME <skj99667788@gmail.com>
2022-10-09 19:19:00 +0500

    updated

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,25 +1,45 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
-  Bars3Icon,
+  Bars3BottomLeftIcon,
   XMarkIcon,
-} from '@heroicons/react/24/outline'
+} from '@heroicons/react/20/solid'
 
-const navigation = [
-  { name: 'Dashboard', href: '#',  current: true },
-  { name: 'Team', href: '#',  current: false },
-  { name: 'Projects', href: '#',  current: false },
-  { name: 'Calendar', href: '#',  current: false },
-  { name: 'Documents', href: '#', current: false },
-  { name: 'Reports', href: '#',  current: false },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function SideBar() {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+const navigation = [
+  { name: 'Home', href: '#', current: false },
+  { name: 'About', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Services', href: '#', current: false},
+  { name: 'Resume', href: '#', current: false },
+  { name: 'Contact', href: '#', current: false },
+]
+
+// current_tab(value) {
+//   // navigation.map(nav => (
+//   //     // If condition
+//   //     // nav.current = value.name === nav.name ? true : false;  //Condition checking if value.type = nav.type 
+//   //     ));
+  
+//       console.log('hello world')
+// }
+
+function tabsactive(value) {
+  navigation.forEach(nav => {
+    // If condition
+    nav.current = value.current === nav.current ? true : false;  //Condition checking if value.type = nav.type
+    console.log(nav.current)
+})
+}
+
+
+  export default function Sidebar() {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [tabscheck, tabsactive] = useState(navigation)
 
   return (
     <>
@@ -48,7 +68,7 @@ export default function SideBar() {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800">
+                <Dialog.Panel className="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-gray-800">
                   <Transition.Child
                     as={Fragment}
                     enter="ease-in-out duration-300"
@@ -58,26 +78,19 @@ export default function SideBar() {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                   >
-                    <div className="absolute top-0 right-0 -mr-12 pt-2">
+                    <div className="absolute top-0 right-0 pt-2 -mr-12">
                       <button
                         type="button"
-                        className="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        className="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                         onClick={() => setSidebarOpen(false)}
                       >
                         <span className="sr-only">Close sidebar</span>
-                        <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                        <XMarkIcon className="w-6 h-6 text-white" aria-hidden="true" />
                       </button>
                     </div>
                   </Transition.Child>
-                  <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-                    <div className="flex flex-shrink-0 items-center px-4">
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
-                      />
-                    </div>
-                    <nav className="mt-5 space-y-1 px-2">
+                  <div className="flex-1 h-0 mt-5 overflow-y-auto">
+                    <nav className="px-2 space-y-1 ">
                       {navigation.map((item) => (
                         <a
                           key={item.name}
@@ -86,7 +99,7 @@ export default function SideBar() {
                             item.current
                               ? 'bg-gray-900 text-white'
                               : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                            'group flex items-center px-2 py-2 text-base font-medium rounded-md self-center justify-center'
                           )}
                         >
                           {item.name}
@@ -94,26 +107,11 @@ export default function SideBar() {
                       ))}
                     </nav>
                   </div>
-                  <div className="flex flex-shrink-0 bg-gray-700 p-4">
-                    <a href="#" className="group block flex-shrink-0">
-                      <div className="flex items-center">
-                        <div>
-                          <img
-                            className="inline-block h-10 w-10 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-base font-medium text-white">Tom Cook</p>
-                          <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">View profile</p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
                 </Dialog.Panel>
               </Transition.Child>
-              <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
+              <div className="flex-shrink-0 w-14" aria-hidden="true">
+                {/* Dummy element to force sidebar to shrink to fit close icon */}
+              </div>
             </div>
           </Dialog>
         </Transition.Root>
@@ -121,70 +119,70 @@ export default function SideBar() {
         {/* Static sidebar for desktop */}
         <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex min-h-0 flex-1 flex-col bg-gray-800">
-            <div className="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
-              <div className="flex flex-shrink-0 items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                  alt="Your Company"
-                />
-              </div>
-              <nav className="mt-5 flex-1 space-y-1 px-2">
+          <div className="flex flex-col flex-1 min-h-0 bg-gray-800">
+            <div data-aos="fade-right" data-aos-duration="2000" className="flex items-center flex-shrink-0 p-2 m-10 bg-gray-900 rounded-full">
+              <img
+                className="rounded-full" 
+                src="https://avatars.githubusercontent.com/u/82492018?v=4"
+                alt="Your Company"
+              />
+            </div>
+            <div className="overflow-x-hidden overflow-y-auto first-letter:">
+              <nav className="flex flex-col items-center px-2 py-4">
                 {navigation.map((item) => (
                   <a
+                  data-aos="fade-left"
+                  data-aos-duration="2000"
+                  // onClick={tabsactive(item)}
                     key={item.name}
                     href={item.href}
                     className={classNames(
                       item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                      'group flex items-center px-10 py-2 text-sm font-medium rounded-md animate__backInDown'
                     )}
                   >
+                  
                     {item.name}
                   </a>
                 ))}
               </nav>
             </div>
-            <div className="flex flex-shrink-0 bg-gray-700 p-4">
-              <a href="#" className="group block w-full flex-shrink-0">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    <p className="text-sm font-medium text-white">Tom Cook</p>
-                    <p className="text-xs font-medium text-gray-300 group-hover:text-gray-200">View profile</p>
-                  </div>
-                </div>
-              </a>
-            </div>
           </div>
         </div>
-        <div className="flex flex-1 flex-col md:pl-64">
-          <div className="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+        <div className="flex flex-col md:pl-64">
+          <div className="sticky top-0 z-10 flex flex-shrink-0 h-16 bg-white shadow">
             <button
               type="button"
-              className="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              className="px-4 text-gray-500 border-r border-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
               onClick={() => setSidebarOpen(true)}
             >
               <span className="sr-only">Open sidebar</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3BottomLeftIcon className="w-6 h-6" aria-hidden="true" />
             </button>
+            <div className="flex justify-between flex-1 px-4">
+              <div className="flex flex-1">
+                <form className="flex w-full md:ml-0" action="#" method="GET">
+                  <label htmlFor="search-field" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
+
           <main className="flex-1">
             <div className="py-6">
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
               </div>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+              <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-4">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+                  <div className="border-4 border-gray-200 border-dashed rounded-lg h-96" />
                 </div>
+                {/* <p class=""> hello world </p> */}
                 {/* /End replace */}
               </div>
             </div>
@@ -194,3 +192,4 @@ export default function SideBar() {
     </>
   )
 }
+
